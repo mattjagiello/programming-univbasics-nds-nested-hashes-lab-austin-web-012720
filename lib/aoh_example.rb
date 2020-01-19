2dbefd3601eefca61a15a1e40115a7af63a0934b
Matthew Jagiello <mjagiello@gmail.com>
2020-01-19 19:52:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,6 @@ def literal_aoh
     PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
     KATE_G = { name:  "Kate Gompert", occupation: "None" },
     BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
-  
   ]
 end
 
